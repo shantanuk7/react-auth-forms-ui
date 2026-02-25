f4fd51141bc166c32902ee51610dc00ca65f2b08
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-25 16:00:27 +0530

    feat: add singup title

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -13,6 +13,7 @@ export default function Signup() {
   return (
     <div className='h-screen flex justify-center items-center'>
         <div className='w-96 mx-auto bg-gray-50 p-8 rounded-2xl shadow-2xs'>
+            <h1 className='text-center font-bold mb-5 text-gray-600'>Signup Form</h1>
             <Formik initialValues={initialValues} validationSchema={signUpValidationSchema} onSubmit={onSubmit} >
                 <Form>
                     <CustomInput type="name" label="Name" name="name" />
