84a2c44eadaf0f811e9b1f0139b6ba22c754f02a
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-25 15:51:19 +0530

    feat: add simple css for signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -11,9 +11,10 @@ export default function Signup() {
 
   const onSubmit = values => { console.log('form data', values) }
   return (
-    <div className='w-96'>
+    <div className='w-96 mx-auto bg-gray-50 p-8 rounded-2xl shadow-2xs'>
         <Formik initialValues={initialValues} validationSchema={signUpValidationSchema} onSubmit={onSubmit} >
             <Form>
+                <CustomInput type="name" label="Name" name="name" />
                 <CustomInput type="email" label="Email" name="email" />
                 <CustomInput type="password" label="Password" name="password" />
                 <button type='submit'>Submit</button>
