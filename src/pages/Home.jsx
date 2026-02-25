7bd9e756549066758e62c8ffb8b49449a540aa0d
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-26 00:16:40 +0530

    feat(Home.jsx): display details of logged in user details on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,25 @@
+import { useContext } from "react"
+import UserContext from "../context/UserContext"
+
 export default function Home(){
-    return <h1>Home page</h1>
+    const {user} = useContext(UserContext);
+    
+    return (
+        <div className="flex bg-emerald-200 h-screen text-xl font-medium justify-center items-center">
+            <div className="w-96 p-10 bg-cyan-50 rounded-lg">
+                {user ?
+                (<div>
+                    <h1>You are logged in:</h1>
+                    <h2>Name: {user.name}</h2>
+                    <h2>Email: {user.email}</h2>
+                </div>
+                ) :
+                (
+                    <div>
+                        Welcome! This is the Homepage!
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
