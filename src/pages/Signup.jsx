c4ff52bea9f9bb90c82ba8c5f35213a2304faab6
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-25 15:59:01 +0530

    feat: add container to signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -11,16 +11,18 @@ export default function Signup() {
 
   const onSubmit = values => { console.log('form data', values) }
   return (
-    <div className='w-96 mx-auto bg-gray-50 p-8 rounded-2xl shadow-2xs'>
-        <Formik initialValues={initialValues} validationSchema={signUpValidationSchema} onSubmit={onSubmit} >
-            <Form>
-                <CustomInput type="name" label="Name" name="name" />
-                <CustomInput type="email" label="Email" name="email" />
-                <CustomInput type="password" label="Password" name="password" />
-                <CustomInput type="password" label="Confirm Password" name="confirmPassword" />
-                <button type='submit' className='bg-amber-400 p-2 mt-2 rounded-md w-full hover:cursor-pointer'>Submit</button>
-            </Form>
-        </Formik>
+    <div className='h-screen flex justify-center items-center'>
+        <div className='w-96 mx-auto bg-gray-50 p-8 rounded-2xl shadow-2xs'>
+            <Formik initialValues={initialValues} validationSchema={signUpValidationSchema} onSubmit={onSubmit} >
+                <Form>
+                    <CustomInput type="name" label="Name" name="name" />
+                    <CustomInput type="email" label="Email" name="email" />
+                    <CustomInput type="password" label="Password" name="password" />
+                    <CustomInput type="password" label="Confirm Password" name="confirmPassword" />
+                    <button type='submit' className='bg-amber-400 p-2 mt-2 rounded-md w-full hover:cursor-pointer'>Submit</button>
+                </Form>
+            </Formik>
+        </div>
     </div>
   )
 }
