826aff048db1b60da24ab66f4372da665cb5c972
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-26 22:58:46 +0530

    refactor: move routes from App.jsx into routes folder

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import CreateTicket from "../pages/CreateTicket";
+import { ProtectedRoute } from "../components/ProtectedRoute";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-ticket" element={<CreateTicket />} />
+      </Route>
+
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
