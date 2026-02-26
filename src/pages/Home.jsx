c39b33feb9f0facab514b2018c3c1954dc885348
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-26 22:50:17 +0530

    feat: add create ticket link in Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,15 @@
-import { useContext } from "react"
-import UserContext from "../context/UserContext"
 import { Link } from "react-router-dom";
 
-export default function Home(){
-    const {user} = useContext(UserContext);
-    
+export default function Home() {
     return (
         <div className="flex bg-emerald-200 h-screen text-xl font-medium justify-center items-center">
             <div className="w-96 p-10 bg-cyan-50 rounded-lg">
-                {user ?
-                (<div>
-                    <h1>You are logged in:</h1>
-                    <h2>Name: {user.name}</h2>
-                    <h2>Email: {user.email}</h2>
+                <div>
+                    Welcome to Homepage! 
+                    <br/>
+                    <Link to={"/create-ticket"} className="text-blue-800">Create Ticket</Link>
                 </div>
-                ) :
-                (
-                    <div>
-                        Welcome! This is the Homepage!
-                        <div>
-                            <Link to={"/signup"}>Signup</Link>
-                            {" "}
-                            <Link to={"/login"}>Login</Link>
-                        </div>
-                    </div>
-                )}
             </div>
         </div>
-    )
+    );
 }
