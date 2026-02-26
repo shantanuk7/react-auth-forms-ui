41f8422ac49e9161fe6b1b8eb782f698460dc8c8
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-26 22:37:17 +0530

    refactor: make a common form component wrapper

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom"
-import LoginForm from "../components/LoginForm"
+import { Link } from "react-router-dom";
+import LoginForm from "../components/LoginForm";
+import Form from "../components/Form";
 
 export default function Login() {
-    return <div className='h-screen flex justify-center items-center'>
-        <div className='w-96 mx-auto bg-gray-50 p-8 rounded-2xl shadow-md border border-slate-100'>
-            <h1 className='text-center font-bold mb-5 text-gray-600'>Login Form</h1>
-            <LoginForm/>
-            <div className="mt-6 text-sm text-slate-700 flex justify-center">
-                <span>Don't have an account?</span>
-                <Link className="text-slate-600 font-bold ml-1" to="/signup">
-                    Signup
-                </Link>
-            </div>
-        </div>
-    </div>
+  return (
+    <Form formTitle="Login Form">
+      <LoginForm />
+      <div className="mt-6 text-sm text-slate-700 flex justify-center">
+        <span>Don't have an account?</span>
+        <Link className="text-slate-600 font-bold ml-1" to="/signup">
+          Signup
+        </Link>
+      </div>
+    </Form>
+  );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
 import SignupForm from "../components/SignupForm";
+import Form from "../components/Form";
 
 export default function Signup() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className='w-96 mx-auto bg-gray-50 p-8 rounded-2xl shadow-md border border-slate-100'>
-        <h1 className="text-center font-bold mb-5 text-gray-600">
-          Signup Form
-        </h1>
+    <Form formTitle="Signup Form">
         <SignupForm />
         <div className="mt-6 text-sm text-slate-700 flex justify-center">
           <span>Already have an account?</span>
@@ -15,7 +12,6 @@ export default function Signup() {
             Login
           </Link>
         </div>
-      </div>
-    </div>
+    </Form>
   );
 }
